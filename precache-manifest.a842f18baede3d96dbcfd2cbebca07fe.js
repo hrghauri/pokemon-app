f176a67369d182a803400cbc0f97d4ab9705692f
Haris Ghauri <hrghauri@hotmail.com>
2019-06-19 04:32:08 -0400

    Updates

---
+++ precache-manifest.a842f18baede3d96dbcfd2cbebca07fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "211d771aa6a87285c837239111153097",
+    "url": "/pokemon-app/index.html"
+  },
+  {
+    "revision": "07fbc34110500515b340",
+    "url": "/pokemon-app/static/css/main.fc7f9ceb.chunk.css"
+  },
+  {
+    "revision": "37a89169e874a2bb463e",
+    "url": "/pokemon-app/static/js/2.f8a9833b.chunk.js"
+  },
+  {
+    "revision": "07fbc34110500515b340",
+    "url": "/pokemon-app/static/js/main.5106becf.chunk.js"
+  },
+  {
+    "revision": "938d9eb8c41100c8aaac",
+    "url": "/pokemon-app/static/js/runtime~main.6b8623f8.js"
+  }
+]);
